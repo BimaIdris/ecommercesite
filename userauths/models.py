1081020f4d7164898a5504bf349180a03251d581
BimaIdris <bimaidris@gmail.com>
2023-11-18 22:00:28 +0100

    Solve Required Problem

--- userauths/models.py
+++ userauths/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username=models.CharField(max_length=100)
+    username=models.CharField(max_length=100,blank=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['username']
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
