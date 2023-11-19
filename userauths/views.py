853d58ee4f888365984b8fed6bd7165eb965c990
BimaIdris <bimaidris@gmail.com>
2023-11-19 10:04:06 +0100

    Frontend Registration Redirection

--- userauths/views.py
+++ userauths/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def register_view(request):
-    form = UserRegisterForm()
-    
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user=form.save()
+            username=form.cleaned_data.get("username")
+            messages.success(request, f"Hello {username}, Welcome")
+            new_user=authenticate(username=form.cleaned_data["email"],
+                                password=form.cleaned_data["password"]
+            )
+            login(request, new_user)
+            return redirect("coreurls:index")
+    else:
+        form = UserRegisterForm()
     context={
         'form': form,
     }
